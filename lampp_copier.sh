39bcf97d23f9ed6e56ffcd16701f07f857d072c5
noobpoodle <32700025+noobpoodle@users.noreply.github.com>
2018-11-23 19:42:54 -0500

    Update lampp_copier.sh

--- lampp_copier.sh
+++ lampp_copier.sh
@@ -8,6 +8,7 @@ LAMPP_UPDATE=$2
 PHPBIN="/opt/lampp"$LAMPP_UPDATE
 
 PHPCPY=$PHPBIN"/bin"
+MODCPY=$PHPBIN"/modules"
 
 PHPEXE="/opt/lampp/bin/php-"$LAMPP_VERSION
 PHPCGI="/opt/lampp/bin/php-cgi-"$LAMPP_VERSION
@@ -50,5 +51,33 @@ echo "input output error, exiting now"
 	exit 1
 fi
 
+LIBPHP4SO="/opt/lampp/modules/libphp4.so"
+LIBPHP5SO="/opt/lampp/modules/libphp5.so"
+LIBPHP7SO="/opt/lampp/modules/libphp7.so"
+
+MODCPY4SO=$MODCPY"/libphp4.so"
+MODCPY5SO=$MODCPY"/libphp5.so"
+MODCPY7SO=$MODCPY"/libphp7.so"
+
+if [[ -f "$MODCPY4SO" || -f "$MODCPY5SO" || -f "$MODCPY7SO" ]]
+	then
+echo "script found module, backing up existing "
+sudo cp $MODCPY4SO $MODCPY"/libphp4.so_" -rf
+sudo cp $MODCPY5SO $MODCPY"/libphp5.so_" -rf
+sudo cp $MODCPY7SO $MODCPY"/libphp7.so_" -rf
+echo "script copying module, versioning now "
+sudo cp $LIBPHP4SO $MODCPY -rf
+sudo cp $LIBPHP5SO $MODCPY -rf
+sudo cp $LIBPHP7SO $MODCPY -rf
+echo "script finished module, backup, copy"
+echo "no changes to /opt/lampp/etc/extra/"
+echo " httpd-xampp.conf (add LoadModule"
+echo " php#_module modules/libphp#.so )"
+	else
+echo "script found missing or no module for php 4,5, or 7, error?"
+	exit 1
+fi
+
 echo "Files copied successfully, no i/o errors,  Done..."
 exit 0
+
